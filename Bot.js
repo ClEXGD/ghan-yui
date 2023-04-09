f36ffa452987dd09a7c8dc172c28ad535fc38ca3
crelgd <maximinplay@gmail.com>
2023-04-09 22:57:20 +0300

    Bot v1.0.0

--- Bot.js
+++ Bot.js
@@ -1,12 +1,39 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();                             let config = require('./botconfig.json');                     let token = config.token;
-let prefix = config.prefix;
+// Импорт модулей из "Discord js"
+const { IntentsBitField, Client } = require('discord.js');
 
+// Создание клиента
+const bot = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ]
+});
+
+// Импорт префикса и токена
+let { prefix, token }= require('./botconfig.json');
+
+// Если бот запущен ...
 bot.on('ready', () => {
         console.log(`Бот Запустился ${bot.user.username}`)
-        });
+});
 
+// Проверка сообщений
 bot.on('mesage', msg => {
-        if (msg.content === 'ping') {
-                msg.reply('Pong!');
-        }                                                            });                                                                                                                  bot.login(token);                                                                                                             bot.login(token);
+    // Если сообщение содержит "ping" ...
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+// Запуск бота
+bot.login(token);
+
+/*
+ * Credits
+ *
+ * Version: v1.0.0
+ * Author: ClEXGD
+ * Editor: crelgd
+*/
